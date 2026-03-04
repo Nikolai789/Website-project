deb50880279c3468d39dd0c44fc05cc6598a9a1a
John Nikolai Camarillo <colaipen8@gmail.com>
2026-03-04 20:13:06 +0800

    added some changes

--- index.php
+++ index.php
@@ -4,15 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Femboy Shop</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <!-- this is our header where the important stuff shows -->
     <header>
         <section>
             <h1>Welcome to femboy order selection</h1>
-            <button>oredr femboys now!!</button>
-            
         </section>
-
     </header>
-</body>
+    <!-- this is our main, where the main stuff resides like the buttons functions and etc -->
+    <main>
+        <section>
+            <div>
+                <h2>shop femboys now!!</h2>
+                <button>shop now</button>
+            </div>
+        </section>
+    </main>
+    <!-- and lastly is our footer where the end stuff is located -->
+    <footer>
+
+    </footer>
 </html>
