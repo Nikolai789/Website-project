8ff8e45ec43b3c83eaa0e95915305e5193fdbfe2
Niko <colaipen8@gmail.com>
2026-03-06 11:08:09 +0800

    major changes

--- about.php
+++ about.php
@@ -17,7 +17,7 @@
     <?php include "includes/header.php" ?>
 
     <main>
-
+        <h2>this is the about page</h2>
     </main>
     
     <?php include "includes/footer.php" ?>
---
+++ includes/header.php
@@ -0,0 +1,6 @@
+    <header>
+        <h1>Welcome to Periph Store</h1>
+        <p class = "subtitle">shop now cuz our shop is full of amazing things</p>
+
+
+    </header>
