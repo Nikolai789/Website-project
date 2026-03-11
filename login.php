86ff7d6542cd4a10fb370a1ad3a5314731d694d0
Niko <colaipen8@gmail.com>
2026-03-11 09:22:21 +0800

    added back button on login/register

--- login.php
+++ login.php
@@ -7,9 +7,11 @@
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
+
     <div class="container">
         <div class="form-box active" id = "login-form">
             <form action="">
+                <a href="index.php" style="text-decoration: none; color: rgba(0,0,0,0.6)">←</a>
                 <h2>Login</h2>
                 <input type="email" name="email" placeholder="email" required>
                 <input type="password" name="password" placeholder="password" required>
@@ -20,6 +22,7 @@
 
         <div class="form-box" id = "register-form">
             <form action="">
+                 <a href="index.php" style="text-decoration: none; color: rgba(0,0,0,0.6)">←</a>
                 <h2>Register</h2>
                 <input type="text" name="username" placeholder="username" required>
                 <input type="email" name="email" placeholder="email" required>
