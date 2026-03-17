acc20f6ea29eb1e820e2d493de62e695b1e4d1ea
Alfred Bisas <bisasalfred@gmail.com>
2026-03-18 00:01:52 +0800

    made a main.css to be different from other css to avoid confusion. Added html and css practice until implementation of database.

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="css/style.css">
     
     <link rel="stylesheet" href="css/header.css">
+
+    <link rel="stylesheet" href="css/main.css">
     <title>Periph</title>
 
 </head>
@@ -23,17 +25,39 @@
     <main>
         <div class="left">
             <div class="section-title">Product Categories</div>
-            <a href="">Mouses</a>
-            <a href="">Keyboards</a>
+            <a href="">Mouse</a>
+            <a href="">Keyboard</a>
         </div>
 
-        <div>
+        <div class="right">
             <div class="section-title">Products</div>
+            <div class="product">
+                <div class="product-left">
+                    <img src="product_images/Mouse.webp" alt="Office Mouse" class="product-img">
+                </div>
+                <div class="product-right">
+                    <p class="name">
+                        <a href="">Logitech M331</a>
+                    </p>
+
+                    <p class="description">The Logitech M331 Silent Plus is a wireless mouse engineered for those who prioritize a quiet, focused workspace without sacrificing performance. Designed with comfort and longevity in mind, it is an ideal companion for busy office environments, shared workspaces, or late-night creative sessions at home.</p>
+                    <div class="price-row">
+                        <p class="price">
+                            <span class="currency">₱</span>930.00 
+                        </p>
+
+                        <p class="stock">
+                            Stock: <span class="yellow">1</span>
+                        </p>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     </main>
     
     <?php include "includes/footer.php" ?>
 
-    <script>src="javascript/script.js"</script>
+    <script src="javascript/script.js"></script>
 </body>
 </html>
