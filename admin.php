af4f24d427e8ecd14ae55aa5075c2d95bfd2a609
John Nikolai Camarillo <colaipen8@gmail.com>
2026-03-19 00:40:59 +0800

    added admin page

---
+++ admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin panel</title>
+</head>
+<body>
+    <header>
+        <h1>Admin panel</h1>
+    </header>
+    <main>
+        <h2>Welcome to the admin panel</h2>
+    </main>
+    <footer>
+        <p>Copyright 2026 GearHub</p>
+    </footer>
+</body>
+</html>
