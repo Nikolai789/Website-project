a6169d79d019957bca0fd9eea230df708f766777
GabBuena01 <buenagabrieljee@gmail.com>
2026-03-20 20:55:05 +0800

    Designed the admin panel

--- configurations/config.php
+++ configurations/config.php
@@ -7,13 +7,8 @@ $database = "gearhubDB";// database name, databse that wel access
 
 $conn = new mysqli($host, $user, $password, $database); // this stores databse connection, it automatically connect to the database
 
-
 // check connection if there are errors, this code runs if there is an error
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-
-
-
 ?>
